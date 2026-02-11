243c9ba82fae9cfaa274e9c91e0a7acc0b1cd534
dwitariska0412-prog <dwitariska0412@gmail.com>
2026-02-11 13:23:45 +0700

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,14 @@
+// Efek muncul saat scroll
+window.addEventListener("scroll", function() {
+    const cards = document.querySelectorAll(".stat-card, .book-card");
+    cards.forEach(card => {
+        const position = card.getBoundingClientRect().top;
+        const screenHeight = window.innerHeight;
+
+        if(position < screenHeight - 100){
+            card.style.opacity = "1";
+            card.style.transform = "translateY(0)";
+        }
+    });
+});
 
